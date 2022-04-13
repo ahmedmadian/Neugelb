e89bf6393dc946a80b24ca5b5d7b04758bc3b247
Ahmed Madian <ahmed.madian@sengaro.com>
2022-04-13 23:43:52 +0200

    init project

---
+++ Movies/Movies/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movies
+//
+//  Created by Ahmed Madian on 13.04.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
