745824c25441fbe605e173151f766a1fbaf90140
Ahmed Madian <ahmed.madian@sengaro.com>
2022-04-13 23:50:53 +0200

    Initial setup

---
+++ Movies/Movies/Application/AppDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.swift
+//  Movies
+//
+//  Created by Ahmed Madian on 13.04.22.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
